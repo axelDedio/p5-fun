6e016a1e4b754299106d3677f5254967b86ea705
Axel Dedio <axel.dedio@bridging-it.de>
2023-01-20 16:21:23 +0100

    integrated sound

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-﻿console.log("hallo");
-import p5 from "p5";
-
-
+﻿import p5, { Oscillator } from "p5";
+import "p5/lib/addons/p5.sound"
 const element = document.querySelector(".centerbox");
 const style = window.getComputedStyle(element);
 const width = parseInt(style.width);
@@ -9,7 +7,7 @@ console.log("width: ", width);
 const height = parseInt(style.height);
 const colors = ['#3b205f', '#6b4b8f', '#9b78bf', '#cea9ef', '#e8d1fb', '#ffbaad', '#f67e84', '#dc4a63', '#ba1d4b', '#93003a', '#3b205f', '#6b4b8f', '#9b78bf', '#cea9ef', '#e8d1fb', '#ffbaad', '#f67e84', '#dc4a63', '#ba1d4b', '#93003a']
 
-const blockCount = 60
+const blockCount = 45
 const blockWhidth = 12
 const border = width - blockWhidth/2;
 const fr = 60;
@@ -21,12 +19,13 @@ const tri = (x, amp, freq) => {
 
 class Block {
   
-  constructor(/** @type {p5} */p, /** @type {number} */i) {
+  constructor(/** @type {p5} */p, /** @type {number} */i, osc) {
     this.p = p;
     this.x =  blockWhidth/2;
     this.i = i;
     this.y = blockWhidth + this.i * 8;
     this.time = 0;
+    this.osc = osc;
   }
   drawBlock(){
       this.p.noStroke();
@@ -35,16 +34,18 @@ class Block {
     }
   moveBlock(){
     this.x = tri(this.p.millis()/1000, border, basefreq*this.i);
-    if(this.x<(blockWhidth/2)+2){
+    if(this.x<3){
       this.x = blockWhidth/2;
+      playOscillator(this.i);
     } 
-    if(this.x>border-2)  {
+    if(this.x>border-3)  {
       this.x = border;
+      playOscillator(this.i);
+      
     }
   }
   colorBlock(){
     let centerDis = this.p.dist(this.x,0,width/2,0)/(width/2);
-    console.log(255*centerDis)
 
     
       this.p.fill(this.p.color(102, 255, 102, 200*centerDis+50));
@@ -54,7 +55,7 @@ class Block {
 
 joinBlocks(blocks) {
   blocks.forEach(element =>{
-    if(Math.abs(this.i - element.i) < 3){
+    if(Math.abs(this.i - element.i) < 2){
       this.p.stroke(this.p.color(102, 255, 102, 50));
       this.p.strokeWeight(1)
       this.p.line(this.x,this.y,element.x,element.y);
@@ -63,21 +64,33 @@ joinBlocks(blocks) {
   });
 }
 }
+function playOscillator(freq) {
+  // starting an oscillator on a user gesture will enable audio
+  // in browsers that have a strict autoplay policy.
+  // See also: userStartAudio();
+  const osc = new Oscillator();
+  osc.start(0, freq*100+300);
+  osc.amp(0, 0.1)
+}
+
 const sketch  = ( /** @type {p5} */p ) => {
-    
   const blocks = [];
-  
+  const osc = new Oscillator()
   p.setup = () => {
+    let cnv = p.createCanvas(width, height);
+    cnv.mousePressed(playOscillator);
+
     p.createCanvas(width, height);
     p.frameRate(fr);
     for(let i = 1;i<blockCount+1;i++){
-      blocks.push(new Block(p, i));
+      blocks.push(new Block(p, i, osc));
     }
  
   };
   
   p.draw = () => {
     p.background(0);
+    
     // p.text(p.millis()/1000, 100, 300);
     for(let i = 0;i<blocks.length;i++) {
       blocks[i].colorBlock();
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+﻿const  webpack  = require("webpack")
+
+module.exports = {
+    plugins: [
+        new webpack.ProvidePlugin({
+        p5: "p5"
+        })
+    ]
+}
