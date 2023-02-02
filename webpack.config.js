0d2e2f07e5953a0d8a230bbcd8b68a3572f5c3a0
Axel Dedio <axel.dedio@bridging-it.de>
2023-02-02 16:26:15 +0100

    remove all unessacary

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 ﻿const  webpack  = require("webpack")
 
 module.exports = {
+    entry: {
+        main: "./src/sketch.js"
+      },
     plugins: [
         new webpack.ProvidePlugin({
         p5: "p5"
