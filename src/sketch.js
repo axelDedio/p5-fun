20dd70b72710b9a34c817341affced0402b3dd57
Axel Dedio <axel.dedio@bridging-it.de>
2023-02-11 16:10:00 +0100

    multiple filled mountains stacked

--- src/sketch.js
+++ src/sketch.js
@@ -6,9 +6,21 @@ const width = parseInt(style.width);
 const height = parseInt(style.height);
 console.log("width: ", width);
 
-const fr = 30;
+const fr = 50;
 
-
+const mountains =  (/** @type {p5} */p, xoff, inc, ysum, color) => {
+    p.fill(p.color(color));
+    p.beginShape(p.LINE);
+    p.vertex(0,height);
+    for(let i = 0; i < width+30; i = i + 1){
+      const y = p.map(p.noise(xoff),0,1,0,80);
+      p.vertex(i, y + ysum[i] )
+      ysum[i]+=y;
+      xoff+=inc
+    }
+    p.vertex(width,height);
+    p.endShape(p.CLOSE);
+}
 
 
 const sketch  = ( /** @type {p5} */p ) => {
@@ -20,18 +32,20 @@ const sketch  = ( /** @type {p5} */p ) => {
   
   
     let start = 0;
-    let inc = 0.04;
+    let inc = 0.008;
     
   p.draw = () => {
     p.background(255)
     p.stroke(100)
     let xoff = start;
+    const ysum = [];
 
-    p.noFill();
+    p.fill(100);
     p.beginShape(p.LINE);
     p.vertex(0,height);
-    for(let i = 0; i < width+30; i = i + 15){
-      const y = p.map(p.noise(xoff),0,1,0,height)
+    for(let i = 0; i < width+30; i = i + 1){
+      const y = p.map(p.noise(xoff),0,1,-200,200)
+      ysum.push(y);
       p.vertex(i, y )
       xoff+=inc
     }
@@ -39,27 +53,14 @@ const sketch  = ( /** @type {p5} */p ) => {
     p.endShape(p.CLOSE);
 
 
-    // p.fill(100);
-    p.beginShape(p.LINE);
-    p.vertex(0,height);
-    for(let i = 0; i < width+30; i = i + 15){
-      const y = p.map(p.noise(xoff),0,1,0,height)
-      p.vertex(i, y )
-      xoff+=inc
-    }
-    p.vertex(width,height);
-    p.endShape(p.CLOSE);
-
-    // p.fill(200);
-    p.beginShape(p.LINE);
-    p.vertex(0,height);
-    for(let i = 0; i < width+30; i = i + 15){
-      const y = p.map(p.noise(xoff),0,1,0,height)
-      p.vertex(i, y )
-      xoff+=inc
-    }
-    p.vertex(width,height);
-    p.endShape(p.CLOSE);
+    mountains(p, xoff, inc, ysum, "red");
+    mountains(p, xoff, inc, ysum, "green");
+    mountains(p, xoff, inc, ysum, "blue");
+    mountains(p, xoff, inc, ysum, "yellow");
+    mountains(p, xoff, inc, ysum, "green");
+    mountains(p, xoff, inc, ysum, "purple");
+    
+ 
     start+=inc
   }
 };
