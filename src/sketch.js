dc4e4331e9ff55018f71bce17e24e55511706560
Axel Dedio <axel.dedio@bridging-it.de>
2023-02-16 16:54:32 +0100

    accellaration impl

--- src/sketch.js
+++ src/sketch.js
@@ -1,6 +1,6 @@
-﻿import p5 from "p5";
+﻿import p5, {Oscillator} from "p5";
 import "p5/lib/addons/p5.sound"
-import { makeNoise3D } from "open-simplex-noise";
+import { makeNoise2D } from "open-simplex-noise";
 
 const element = document.querySelector(".centerbox");
 const style = window.getComputedStyle(element);
@@ -9,9 +9,44 @@ const height = parseInt(style.height);
 console.log("width: ", width);
 console.log("height: ", height);
 
-const fr = 40;
-const noise3D = makeNoise3D(Date.now()); 
+const noise3D = makeNoise2D(Date.now()); 
 
+const fr = 40;
+const inc = 0.03;
+const zinc = 0.05;
+let zoff = 0;
+
+class Mover {
+  constructor(/** @type {p5} */p ){
+    this.p = p;
+    /** @type {p5.Vector} */this.location = new p.createVector(0, 0);
+    /** @type {p5.Vector} */this.velocity = new p.createVector(10,0);
+    /** @type {p5.Vector} */this.accell = new p.createVector(0,0.9);
+    this.osc = new Oscillator();
+    this.osc.start(0, 300);
+  };
+  display() {
+    this.p.ellipse(this.location.x , this.location.y,  20);
+  }
+
+  update() {
+    this.velocity.add(this.accell)
+    this.location.add(this.velocity);
+    this.osc.freq(this.p.map(this.location.y, 0, height, 500,300))
+    
+  }
+  handleEdge(){
+    if((this.location.x > width) || (this.location.x < 0)) {
+      this.velocity.x = this.velocity.x * -1;
+    }
+    if((this.location.y > height  ) || (this.location.y < 0)) {
+      
+      this.velocity.y = this.velocity.y * -1;
+
+    }
+  }
+
+}
 
 
 const sketch  = ( /** @type {p5} */p ) => {
@@ -19,35 +54,21 @@ const sketch  = ( /** @type {p5} */p ) => {
     p.createCanvas(width, height);
     p.background(255);
     p.frameRate(fr);
+    
   };
   
-const inc = 0.03;
-const zinc = 0.05;
-let zoff = 0;
-p.noiseDetail(0)
-  p.draw = () => {
-  
-    p.loadPixels();
-    p.pixelDensity(1);
-    let yoff = 0;
-    for(let y = 0; y < height; y++){
-      let xoff = 0;
-      for(let x = 0; x < width; x++){ 
-        const index = (x + y * width) * 4;
-        const rd = noise3D(xoff , yoff, zoff);
-        const bla = p.map(rd,-1, 1, 0,255 )
-        p.pixels[index] = 255;
-        p.pixels[index + 1] = 255;
-        p.pixels[index + 2] = 255;
-        p.pixels[index + 3] = bla;
-        xoff+=inc;
-        }
-      
-      yoff+=inc;
-    }
-    zoff+=zinc
 
-    p.updatePixels()
+const mover = new Mover(p)
+  p.draw = () => {
+
+    mover.display();
+    mover.update();
+    mover.handleEdge();
+   
   };
+  p.mousePressed = () => {
+    p.userStartAudio();
+  }
 };
+
 let myp5 = new p5(sketch, document.querySelector('.centerbox'));
