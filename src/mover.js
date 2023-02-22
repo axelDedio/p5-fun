6aeac52123b6ec64c6b61944fe357a03d46a4671
Axel Dedio <axel.dedio@bridging-it.de>
2023-02-22 11:46:44 +0100

    export mover and drag force

---
+++ src/mover.js
@@ -0,0 +1,82 @@
+import p5 from "p5";
+const element = document.querySelector(".centerbox");
+const style = window.getComputedStyle(element);
+const width = parseInt(style.width);
+const height = parseInt(style.height);
+export const dims = { width, height };
+
+export default class Mover {
+    constructor(/** @type {p5} */ p, r, color) {
+        this.p = p;
+        /** @type {p5.Vector} */ this.location = new p.createVector(10, 10);
+        /** @type {p5.Vector} */ this.velocity = new p.createVector(2, 0);
+        /** @type {p5.Vector} */ this.accell = new p.createVector(0, 0);
+        this.r = r;
+        this.mass = 0.01 * (this.p.PI * this.p.sq(this.r));
+        console.log(this.mass);
+        this.color = color;
+    }
+    display() {
+        this.p.fill(this.p.color(this.color));
+        this.p.ellipse(this.location.x, this.location.y, this.r * 2);
+    }
+
+    update() {
+        this.p.noStroke();
+        this.velocity.add(this.accell);
+        this.velocity.limit(10);
+        this.location.add(this.velocity);
+        this.accell.mult(0);
+    }
+
+    applyForce(/** @type {p5.Vector} */ force) {
+        let f = p5.Vector.div(force, this.mass);
+        this.accell.add(f);
+    }
+    handleDrag() {
+        if (this.location.x > width / 2) {
+            const direction = this.velocity.copy();
+            direction.normalize();
+            direction.mult(-1);
+            direction.mult(this.velocity.magSq());
+            direction.mult(0.3);
+            this.applyForce(direction);
+        }
+    }
+    handleEdge() {
+        if (this.location.x > width - this.r) {
+            this.location.x = width - this.r;
+            this.velocity.x *= -1;
+        }
+        if (this.location.x < this.r) {
+            this.location.x = this.r;
+            this.velocity.x *= -1;
+        }
+        if (this.location.y > height - this.r) {
+            this.location.y = height - this.r;
+            this.velocity.y *= -1;
+        }
+        if (this.location.y < this.r) {
+            this.location.y = this.r;
+            this.velocity.y *= -1;
+        }
+    }
+    drawVectors() {
+        this.p.stroke(this.p.color("yellow"));
+
+        this.p.line(
+            this.location.x,
+            this.location.y,
+            this.location.x + this.velocity.x * 5,
+            this.location.y + this.velocity.y * 5
+        );
+        this.p.stroke(this.p.color("green"));
+
+        this.p.line(
+            this.location.x,
+            this.location.y,
+            this.location.x + this.accell.x * 100,
+            this.location.y + this.accell.y * 100
+        );
+    }
+}
--- src/sketch.js
+++ src/sketch.js
@@ -1,82 +1,51 @@
-import p5, { Oscillator } from "p5";
+import p5 from "p5";
 import "p5/lib/addons/p5.sound";
-import { makeNoise2D } from "open-simplex-noise";
-
-const element = document.querySelector(".centerbox");
-const style = window.getComputedStyle(element);
-const width = parseInt(style.width);
-const height = parseInt(style.height);
-
+import Mover, { dims } from "./mover";
+const width = dims.width;
+const height = dims.height;
 console.log("width: ", width);
-
 console.log("height: ", height);
 
-// const noise3D = makeNoise2D(Date.now());
-
-const fr = 40;
-
-const osc = new Oscillator();
-osc.amp(0.1);
-class Mover {
-  constructor(/** @type {p5} */ p, osc) {
-    this.p = p;
-    /** @type {p5.Vector} */ this.location = new p.createVector(10, 10);
-    /** @type {p5.Vector} */ this.velocity = new p.createVector(1, 1);
-    /** @type {p5.Vector} */ this.accell = new p.createVector(0, 0);
-    this.osc = osc;
-    this.osc.setType("square");
-  }
-  display() {
-    this.p.ellipse(this.location.x, this.location.y, 20);
-  }
-
-  update() {
-    const mouse = this.p.createVector(this.p.mouseX, this.p.mouseY);
-    this.accell = p5.Vector.sub(mouse, this.location);
-    this.accell.setMag(1);
-    this.velocity.add(this.accell);
-    this.velocity.limit(20);
-    this.location.add(this.velocity);
-    this.osc.freq(this.p.map(this.velocity.mag(), 0, 20, 100, 200));
-  }
-  handleEdge() {
-    if (this.location.x > width || this.location.x < 0) {
-      this.velocity.x = this.velocity.x * -1;
-    }
-    if (this.location.y > height || this.location.y < 0) {
-      this.velocity.y = this.velocity.y * -1;
-    }
-  }
-}
+const fr = 80;
 
 const sketch = (/** @type {p5} */ p) => {
-  p.setup = () => {
-    console.log("h");
+    const moverA = new Mover(p, 10, "red");
+    const moverB = new Mover(p, 15, "blue");
+    const moverC = new Mover(p, 20, "green");
+    const movers = [];
+    movers.push(moverA);
+    movers.push(moverB);
+    movers.push(moverC);
 
-    p.createCanvas(width, height);
-    p.background(255);
-    p.frameRate(fr);
-  };
+    p.setup = () => {
+        console.log("h");
 
-  const mover = new Mover(p, osc);
-  p.draw = () => {
-    mover.display();
-    mover.update();
+        p.createCanvas(width, height);
+        p.frameRate(fr);
+    };
 
-    mover.handleEdge();
-  };
-  let playing = false;
-  p.mousePressed = () => {
-    console.log("hallo");
-    if (playing) {
-      osc.stop();
-      playing = false;
-    } else {
-      p.userStartAudio();
-      osc.start();
-      playing = true;
-    }
-  };
+    p.draw = () => {
+        const mag = 1;
+        const gravity = 0.2;
+        p.createCanvas(width, height);
+        p.background(255);
+        p.fill(p.color("grey"));
+        p.rect(width / 2, 0, width / 2, height);
+        for (let mover of movers) {
+            if (p.mouseIsPressed) {
+                let mouse = p.createVector(p.mouseX, p.mouseY);
+                let force = p5.Vector.sub(mouse, mover.location);
+                force.setMag(mag);
+                mover.applyForce(force);
+            }
+            mover.display();
+            mover.drawVectors();
+            mover.handleDrag();
+            mover.update();
+            mover.handleEdge();
+            mover.applyForce(new p.createVector(0, gravity).mult(mover.mass));
+        }
+    };
 };
 
 let myp5 = new p5(sketch, document.querySelector(".centerbox"));
