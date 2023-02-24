d55403fc973ef84635f7607eef3738287b4bf8b8
Axel Dedio <axel.dedio@bridging-it.de>
2023-02-24 12:15:40 +0100

    attractor lines

--- src/mover.js
+++ src/mover.js
@@ -4,16 +4,16 @@ const style = window.getComputedStyle(element);
 const width = parseInt(style.width);
 const height = parseInt(style.height);
 export const dims = { width, height };
+const G = 3;
 
 export default class Mover {
-    constructor(/** @type {p5} */ p, r, color) {
+    constructor(/** @type {p5} */ p, r, color, px, py, vx, vy) {
         this.p = p;
-        /** @type {p5.Vector} */ this.location = new p.createVector(10, 10);
-        /** @type {p5.Vector} */ this.velocity = new p.createVector(2, 0);
-        /** @type {p5.Vector} */ this.accell = new p.createVector(0, 0);
         this.r = r;
         this.mass = 0.01 * (this.p.PI * this.p.sq(this.r));
-        console.log(this.mass);
+        /** @type {p5.Vector} */ this.location = new p.createVector(px, py);
+        /** @type {p5.Vector} */ this.velocity = new p.createVector(vx, vy);
+        /** @type {p5.Vector} */ this.accell = new p.createVector(0, 0);
         this.color = color;
     }
     display() {
@@ -23,14 +23,33 @@ export default class Mover {
 
     update() {
         this.velocity.add(this.accell);
-        this.velocity.limit(10);
+        this.velocity.limit(15);
         this.location.add(this.velocity);
         this.accell.mult(0);
     }
 
+    calculateAttraction(object) {
+        const attractorMass = object.mass;
+        const dir = p5.Vector.sub(this.location, object.location).mult(-1);
+        const magSq = this.p.constrain(dir.magSq(), 5, 25);
+        const strength = (this.mass * attractorMass * G) / magSq;
+        dir.normalize();
+        dir.setMag(strength);
+        this.applyForce(dir);
+    }
+
     applyForce(/** @type {p5.Vector} */ force) {
         let f = p5.Vector.div(force, this.mass);
-        this.accell.add(f);
+        this.accell.add(force);
+    }
+
+    handleDrag() {
+        const direction = this.velocity.copy();
+        direction.normalize();
+        direction.mult(-1);
+        direction.mult(this.velocity.magSq());
+        direction.mult(0.001);
+        this.applyForce(direction);
     }
 
     handleEdge() {
