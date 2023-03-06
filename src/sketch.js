57ad7384678b8bcacfad7f2b6d7d236b15fb7b95
Axel Dedio <axel.dedio@bridging-it.de>
2023-03-06 16:09:57 +0100

    spermmover

--- src/sketch.js
+++ src/sketch.js
@@ -1,5 +1,6 @@
 ﻿import p5 from "p5";
 import "p5/lib/addons/p5.sound";
+import { FunnyMover } from "./funnyMover";
 import { width, height } from "./helper";
 
 const fr = 60;
@@ -10,29 +11,20 @@ const sketch = (/** @type {p5} */ p) => {
         p.background(0);
         p.frameRate(fr);
     };
-    let a = 0;
-    let d = 1;
-
+    const spacecraft = new FunnyMover(p);
+    const fish = [];
     p.draw = () => {
         p.background(0);
-        p.translate(width / 2, height / 2);
-        p.stroke(255);
-        for (let i = 0; i < 500; i += 5) {
-            a += (p.PI / 5000) * d;
-            // if (a >= p.PI) {
-            //     d = -d;
-            // }
-            // if (a <= 0) {
-            //     d = -d;
-            // }
-            p.circle(p.cos(a * (i / 200)) * i, p.sin(a * (i / 200)) * i, 5);
-            // p.line(
-            //     p.cos(a * (i / 200)) * i,
-            //     p.sin(a * (i / 200)) * i,
-            //     p.cos(a * ((i - 5) / 200)) * (i - 5),
-            //     p.sin(a * ((i - 5) / 200)) * (i - 5)
-            // );
+        // p.translate(width / 2, height / 2);
+        spacecraft.display();
+        spacecraft.update();
+        if (p.mouseIsPressed) {
+            let mouse = p.createVector(p.mouseX, p.mouseY);
+            let force = mouse.sub(spacecraft.location);
+            force.setMag(0.1);
+            spacecraft.applyForce(force);
         }
+        spacecraft.handleEdge();
     };
 };
 
