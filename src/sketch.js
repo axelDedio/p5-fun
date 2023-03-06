d17274c8f9d5c0e91d73b651640509b5f10aa4f4
Axel Dedio <axel.dedio@bridging-it.de>
2023-03-06 16:29:42 +0100

    oscillator class

--- src/sketch.js
+++ src/sketch.js
@@ -2,6 +2,7 @@
 import "p5/lib/addons/p5.sound";
 import { FunnyMover } from "./funnyMover";
 import { width, height } from "./helper";
+import { Oscillator } from "./oscillator";
 
 const fr = 60;
 
@@ -11,20 +12,25 @@ const sketch = (/** @type {p5} */ p) => {
         p.background(0);
         p.frameRate(fr);
     };
-    const spacecraft = new FunnyMover(p);
+    const wave = new Oscillator(p, 200, 0, width);
     const fish = [];
     p.draw = () => {
         p.background(0);
-        // p.translate(width / 2, height / 2);
-        spacecraft.display();
-        spacecraft.update();
-        if (p.mouseIsPressed) {
-            let mouse = p.createVector(p.mouseX, p.mouseY);
-            let force = mouse.sub(spacecraft.location);
-            force.setMag(0.1);
-            spacecraft.applyForce(force);
+        p.translate(0, height / 2);
+        for (let x = 0; x < width; x += 5) {
+            p.circle(x, wave.get(x), 5);
+            wave.update();
         }
-        spacecraft.handleEdge();
+
+        // spacecraft.display();
+        // spacecraft.update();
+        // if (p.mouseIsPressed) {
+        //     let mouse = p.createVector(p.mouseX, p.mouseY);
+        //     let force = mouse.sub(spacecraft.location);
+        //     force.setMag(0.1);
+        //     spacecraft.applyForce(force);
+        // }
+        // spacecraft.handleEdge();
     };
 };
 
