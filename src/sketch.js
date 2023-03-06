38d5d727907c53946f7cac671e737f0f69064b24
Axel Dedio <axel.dedio@bridging-it.de>
2023-03-06 09:52:16 +0100

    inandout

--- src/sketch.js
+++ src/sketch.js
@@ -10,24 +10,28 @@ const sketch = (/** @type {p5} */ p) => {
         p.background(0);
         p.frameRate(fr);
     };
-    const r = height / 4;
+    let offR = 0;
+    let off = 0;
     p.draw = () => {
         p.background(0);
         p.translate(width / 2, height / 2);
-        // p.rotate(-p.PI / 2);
+        p.rotate(-p.PI / 2);
         p.noFill();
         p.stroke(255);
-        let sides = 20;
-        let amp = p.map(p.mouseX, 0, width, 2, 200);
-        p.beginShape();
-        for (let a = 0; a < p.TAU; a += 0.01) {
-            let vex = p.createVector(p.cos(a) * r, p.sin(a) * r);
-            // vex.normalize();
-            vex.setMag(vex.mag() + p.sin(a * amp) * 20);
-            p.line(0, 0, vex.x, vex.y);
-            p.vertex(vex.x, vex.y);
+        let rf = p.map(p.sin(offR), -1, 1, 0, 20);
+        for (let r = 0; r < 100; r += 10) {
+            p.beginShape();
+            for (let a = 0; a < p.TAU; a += 0.03) {
+                let vex = p.createVector(p.cos(a) * r * rf, p.sin(a) * r * rf);
+                // vex.normalize();
+                vex.setMag(vex.mag() + p.sin(a * 8) * 20);
+                // p.line(0, 0, vex.x, vex.y);
+                p.vertex(vex.x, vex.y);
+            }
+            p.endShape("close");
         }
-        p.endShape("close");
+        off += 0.2;
+        offR += 0.02;
     };
 };
 
