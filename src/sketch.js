99c7e3a49b4740e0da500f5cd84db1508ed383af
Axel Dedio <axel.dedio@bridging-it.de>
2023-03-06 10:48:01 +0100

    coolspiral

--- src/sketch.js
+++ src/sketch.js
@@ -10,28 +10,29 @@ const sketch = (/** @type {p5} */ p) => {
         p.background(0);
         p.frameRate(fr);
     };
-    let offR = 0;
-    let off = 0;
+    let a = 0;
+    let d = 1;
+
     p.draw = () => {
         p.background(0);
         p.translate(width / 2, height / 2);
-        p.rotate(-p.PI / 2);
-        p.noFill();
         p.stroke(255);
-        let rf = p.map(p.sin(offR), -1, 1, 0, 20);
-        for (let r = 0; r < 100; r += 10) {
-            p.beginShape();
-            for (let a = 0; a < p.TAU; a += 0.03) {
-                let vex = p.createVector(p.cos(a) * r * rf, p.sin(a) * r * rf);
-                // vex.normalize();
-                vex.setMag(vex.mag() + p.sin(a * 8) * 20);
-                // p.line(0, 0, vex.x, vex.y);
-                p.vertex(vex.x, vex.y);
-            }
-            p.endShape("close");
+        for (let i = 0; i < 500; i += 5) {
+            a += (p.PI / 5000) * d;
+            // if (a >= p.PI) {
+            //     d = -d;
+            // }
+            // if (a <= 0) {
+            //     d = -d;
+            // }
+            p.circle(p.cos(a * (i / 200)) * i, p.sin(a * (i / 200)) * i, 5);
+            // p.line(
+            //     p.cos(a * (i / 200)) * i,
+            //     p.sin(a * (i / 200)) * i,
+            //     p.cos(a * ((i - 5) / 200)) * (i - 5),
+            //     p.sin(a * ((i - 5) / 200)) * (i - 5)
+            // );
         }
-        off += 0.2;
-        offR += 0.02;
     };
 };
 
