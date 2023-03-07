2174fab3da4c67d98fe32c649629962e0595f4a2
Axel Dedio <axel.dedio@bridging-it.de>
2023-03-07 19:26:04 +0100

    linewaves

--- src/sketch.js
+++ src/sketch.js
@@ -14,45 +14,24 @@ const sketch = (/** @type {p5} */ p) => {
         p.background(0);
         p.frameRate(fr);
     };
-    const wave = new Oscillator(p, 20, 20, 2000);
-    const fish = [];
-    let off = 0;
-    let ampOff = 0;
-    const freq = 0.5;
     let roff = 0;
-    const lines = 30;
+    let off = 0;
     p.draw = () => {
         p.background(0);
-        p.rotateX(p.PI / 3);
-        p.translate(-width / 2, -height / 2);
+        p.rotateX(p.PI / 2);
+        p.rotateZ(-p.PI / 1.5);
+        // p.rotateY(-p.PI / 4);
+
         p.noFill();
         p.stroke(p.color("white"));
-        for (let i = 0.5; i <= 20; i++) {
-            p.beginShape();
-            for (let x = 0; x <= width; x += 1) {
-                p.vertex(
-                    x,
-                    i * 20 +
-                        p.map(p.sin((i * p.TAU * x) / width), -1, 1, -off, off),
-                    5
-                );
-                ampOff += 0.01;
-                off = p.map(p.sin((p.TAU * ampOff) / 6000), -1, 1, -5, 5);
-            }
-            p.endShape();
+        for (let i = -800; i < 800; i += 5) {
+            let dz = p.sin(roff + i / 200) * 200;
+            let dy = p.cos(roff + i / 200) * 200;
+            p.stroke(255, 255, 255, i);
+            p.line(-1000, i, dy, 1000, i, dy);
         }
-        console.log(p.frameRate());
-        // roff += 0.001;
-
-        // spacecraft.display();
-        // spacecraft.update();
-        // if (p.mouseIsPressed) {
-        //     let mouse = p.createVector(p.mouseX, p.mouseY);
-        //     let force = mouse.sub(spacecraft.location);
-        //     force.setMag(0.1);
-        //     spacecraft.applyForce(force);
-        // }
-        // spacecraft.handleEdge();
+        roff -= 0.01;
+        // off -= 0.001;
     };
 };
 
