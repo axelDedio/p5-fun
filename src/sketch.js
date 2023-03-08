7cb1320edf068345da3d19df4786db96ee3b2d18
Axel Dedio <axel.dedio@bridging-it.de>
2023-03-08 10:19:52 +0100

    camera

--- src/sketch.js
+++ src/sketch.js
@@ -7,31 +7,46 @@ import { Oscillator } from "./oscillator";
 const width = 500;
 const height = 500;
 const fr = 60;
-
+let sliderX;
+let sliderY;
+let sliderZ;
 const sketch = (/** @type {p5} */ p) => {
     p.setup = () => {
         p.createCanvas(width, height, p.WEBGL);
         p.background(0);
         p.frameRate(fr);
+        sliderX = p.createSlider(-10000, 10000, 0);
+        sliderY = p.createSlider(-10000, 10000, 0);
+        sliderZ = p.createSlider(-10000, 10000, 0);
     };
     let roff = 0;
     let off = 0;
     p.draw = () => {
         p.background(0);
-        p.rotateX(p.PI / 2);
-        p.rotateZ(-p.PI / 1.5);
+        p.camera(
+            sliderX.value(),
+            sliderY.value(),
+            sliderZ.value(),
+            0,
+            0,
+            0,
+            0,
+            1,
+            0
+        );
+        // p.rotateX(p.PI / 3);
+        // p.rotateZ(-p.PI / 1.5);
         // p.rotateY(-p.PI / 4);
 
         p.noFill();
         p.stroke(p.color("white"));
-        for (let i = -800; i < 800; i += 5) {
-            let dz = p.sin(roff + i / 200) * 200;
-            let dy = p.cos(roff + i / 200) * 200;
+        for (let i = -800; i < 800; i += 10) {
+            let dz = p.cos(roff + i / 100) * 200;
             p.stroke(255, 255, 255, i);
-            p.line(-1000, i, dy, 1000, i, dy);
+            p.line(-8000, i, dz, 8000, i, dz);
         }
-        roff -= 0.01;
-        // off -= 0.001;
+        roff -= 0.1;
+        off += 10;
     };
 };
 
