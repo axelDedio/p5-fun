124c9bd15adf9acf877c0e92d18db4fd94497592
Axel Dedio <axel.dedio@bridging-it.de>
2023-03-12 10:26:32 +0100

    fluidboxes attempt

--- src/box.js
+++ src/box.js
@@ -1,9 +1,31 @@
 import p5 from "p5";
-export class WaveGenerator {
-    constructor(/** @type {p5} */ p, amp, period, size) {
+export class Box {
+    constructor(/** @type {p5} */ p, x, y, initZ, initVel, initAcc) {
         /** @type {p5} */ this.p = p;
+        this.x = x;
+        this.y = y;
+        this.z = initZ;
+        this.zVel = initVel;
+        this.zAcc = initAcc;
     }
 
-    get() {}
-    update() {}
+    update() {
+        this.zVel += this.zAcc;
+        this.z += this.zVel;
+        if (this.z < 10) {
+            this.z = 10;
+            this.zVel = 0;
+            this.zAcc = 0;
+        }
+    }
+
+    applyForce(g) {
+        this.zAcc += g;
+    }
+    display() {
+        this.p.push();
+        this.p.translate(this.x * 5, this.y * 5, this.z / 2);
+        this.p.box(5, 5, this.z);
+        this.p.pop();
+    }
 }
--- src/sketch.js
+++ src/sketch.js
@@ -1,39 +1,45 @@
 ﻿import p5 from "p5";
+import { Box } from "./box";
 import { width, height } from "./helper";
-import NoiseGenerator from "./noiseGenerator";
-import { WaveGenerator } from "./waveGenerator";
 
-const fr = 60;
+const fr = 50;
 let slider;
 const sketch = (/** @type {p5} */ p) => {
     p.setup = () => {
         p.createCanvas(width, height, p.WEBGL);
         p.background(0);
         p.frameRate(fr);
-        slider = p.createSlider(0, p.PI / 2, p.PI / 2, 0.01);
+        slider = p.createSlider(0, p.PI / 2, p.PI / 3, 0.01);
     };
+    const boxes = [];
+    for (let y = 0; y < 2; y++) {
+        let boxRow = [];
+        for (let x = 0; x <= 100; x++) {
+            if (x == 0) {
+                boxRow.push(new Box(p, x, y, 10, 0, 0.1));
+            } else {
+                boxRow.push(new Box(p, x, y, 10, 0, 0));
+            }
+        }
+        boxes.push(boxRow);
+    }
 
-    // const noiseGen = new NoiseGenerator(p, 200, 200);
-    const waveGen = new WaveGenerator(p, 1, 40, 160);
     p.draw = () => {
         p.background(0);
         p.rotateX(slider.value());
-        p.translate(-400, -25);
-        for (let y = 0; y < 5; y++) {
-            for (let x = 0; x <= 160; x++) {
-                let nv = p.map(waveGen.get(x), -1, 1, 0, 30);
-                // nv += p.map(waveGen2.get(x), -1, 1, 0, 30);
-                // nv += p.map(waveGen3.get(x), -1, 1, 0, 30);
-                p.fill(255, p.map(nv, 0, 100, 0, 255), 0, 255);
-                // p.noStroke();
-                p.push();
-                p.translate(x * 5, y * 5, nv / 2);
-                p.box(5, 5, nv);
-                p.pop();
-            }
-        }
+        p.translate(-250, -25);
+        boxes.forEach((boxRow) => {
+            boxRow.forEach((box, index, br) => {
+                let next = br[index + 1];
+                box.applyForce(-0.0001);
+                box.update();
+                box.display();
+                if (index < br.length - 1) {
+                    box.applyForce(next.zAcc - box.zAcc);
+                }
+            });
+        });
 
-        waveGen.update(50);
         if (p.frameCount % 120 == 0) {
             console.log(p.frameRate());
         }
