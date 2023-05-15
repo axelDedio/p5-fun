29a900d3a03c756384fa24b06ccdb6220effbd58
Axel Dedio <axel.dedio@bridging-it.de>
2023-05-15 18:01:14 +0200

    shader setup

--- src/sketch.js
+++ src/sketch.js
@@ -1,62 +1,27 @@
 ﻿import p5 from "p5";
 import { width, height } from "./helper";
-import NoiseGenerator from "./noiseGenerator";
-import Particle from "./particle";
-import { WaveGenerator } from "./waveGenerator";
 
-const fr = 60;
-let slider;
-let sliderInc;
-const particles = [];
+import vert from "./shader.vert";
+import frag from "./shader.frag";
 let frp;
+
 const sketch = (/** @type {p5} */ p) => {
-    const boxSize = 20;
-    const noiseGen = new NoiseGenerator(p, boxSize);
-    const points = [];
+    let theShader;
 
     p.setup = () => {
         p.createCanvas(width, height, p.WEBGL);
-        p.background(255);
-        p.frameRate(fr);
-        sliderInc = p.createSlider(0.0001, 0.8, 0.2, 0.0001);
+        theShader = p.createShader(vert, frag);
+        p.shader(theShader);
         frp = p.createP("");
-        for (let y = 0; y < height / boxSize; y++) {
-            for (let x = 0; x < width / boxSize; x++) {
-                points.push({ x, y });
-            }
-        }
-    };
-    const mapNoise = (x, y) => {
-        return p.map(noiseGen.getNoise(x, y), -1, 1, 0, 100);
+        p.noStroke();
     };
     p.draw = () => {
-        p.background(255, 2);
-        p.translate(-width / 2, -height / 2);
-        points.forEach((point, i, ps) => {
-            let nv = mapNoise(point.x, point.y);
-            p.push();
-            p.translate(point.x * boxSize, point.y * boxSize);
-            p.strokeWeight(2);
-            p.strokeCap("ROUND");
-            p.point(0, 0, nv);
-            p.pop();
-        });
-        // particles.forEach((particle) => {
-        //     particle.update();
-        //     particle.display();
-        //     particle.handleEdge();
-        //     let ni = mapNoise(
-        //         p.round(particle.location.x / boxSize),
-        //         p.round(particle.location.y / boxSize)
-        //     );
-        //     let force = p5.Vector.fromAngle(ni);
-        //     force.mult(2);
-        //     particle.applyForce(force);
-        // });
-
-        noiseGen.update(sliderInc.value());
+        theShader.setUniform("u_resolution", [width, height]);
+        theShader.setUniform("mouseX", p.map(p.mouseX, 0, width, 0, 1));
+        theShader.setUniform("mouseY", p.map(p.mouseY, 0, width, 0, 1));
+        p.rect(1000, 1000, 10, 10);
         if (p.frameCount % 10 == 0) {
-            frp.html(`${p.floor(p.frameRate())}, ${sliderInc.value()}`);
+            frp.html(`${p.floor(p.frameRate())}`);
         }
     };
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
-﻿const  webpack  = require("webpack")
+﻿const webpack = require("webpack");
 
 module.exports = {
     entry: {
-        main: "./src/sketch.js"
-      },
+        main: "./src/sketch.js",
+    },
     plugins: [
         new webpack.ProvidePlugin({
-        p5: "p5"
-        })
-    ]
-}
+            p5: "p5",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(vert|frag)$/,
+                use: "raw-loader",
+            },
+        ],
+    },
+};
